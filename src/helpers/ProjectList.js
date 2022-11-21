dab8a725af3bf08152854d2b353642aef2849c34
yeasirh <yeasirh@gmail.com>
2022-11-21 12:35:52 +1100

    projects section added

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,21 @@
+import houdini from "../assets/houdini.png";
+import killara from "../assets/killara.png";
+
+export const projectList = [
+	{
+		name: "Killara",
+		description:
+			"A distraction-free writing application designed for developers",
+		skills:
+			"React, Tauri, Mantine, Iconoir, TypeScript, Sass, Dependabot, CODEOWNERS",
+		repo: "https://github.com/eungella-io/Killara",
+		image: killara,
+	},
+	{
+		name: "Houdini:",
+		description: "A collection of web tools for developers.",
+		skills: "React, Mantine, Iconoir, TypeScript, Sass, Dependabot, CODEOWNERS",
+		repo: "https://github.com/lifeparticle/Houdini",
+		image: houdini,
+	},
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { projectList } from "../helpers/ProjectList";
+import ProjectItem from "../components/ProjectItem";
+
+import "../styles/Projects.css";
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+	return (
+		<div className="projects">
+			<h1>My personal projects</h1>
+			<div className="projectList">
+				{projectList.map((project, idx) => {
+					return (
+						<ProjectItem id={idx} name={project.name} image={project.image} />
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default Projects
+export default Projects;
